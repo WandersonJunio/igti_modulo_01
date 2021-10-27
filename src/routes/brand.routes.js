09763dd3194f41df2bb0fde73a3f5baa30960f33
WandersonJunio <wanderson.junioantunes@outlook.com>
2021-10-26 21:09:00 -0300

    Criacao de nova rota

--- src/routes/brand.routes.js
+++ src/routes/brand.routes.js
@@ -9,23 +9,61 @@ route.get("/maisModelos", async (req, res, next) => {
   try {
     const carList = JSON.parse(await readFile("car-list.json"));
     
-    const moreModels = carList.map((brand) => {
-      var response;
-      for (var i=0; i < brand.models.length; i++) {
-        console.log(brand.models[i])
-
+    var moreModels = [];
+    for (var i=0; i < Object.keys(carList).length; i++) {
+      if (response) {
+        if (Object.keys(response).length > 0) {
+          if (response['total'] <= carList[i].models.length) {
+            response['total'] = carList[i].models.length;
+            response['marca'] = carList[i].brand;
+          }
+          continue;
+        }
       }
-      // for (i in brand.models) {
-      //   // response[brand.models[i]] += brand.models[i];
 
-      // };
-    })
+      var response = new Object();
+      response['total'] = carList[i].models.length;
+      response['marca'] = carList[i].brand;
+    };
 
-    console.log();
+    moreModels.push(response);
+    res.json(moreModels);
   } catch (error) {
     res.json({message: error})
     next();
   }
+});
+
+route.get("/menosModelos", async (req, res, next) => {
+  try {
+    const carList = JSON.parse(await readFile("car-list.json"));
+    var moreModels= [];
+    for (var i=0; i < Object.keys(carList).length; i++) {
+      if (response) {
+        if (Object.keys(response).length > 0) {
+          if (carList[i].models.length < response['total']) {
+            response['total'] = carList[i].models.length;
+            response['marca'] = carList[i].brand;
+          }
+          continue;
+        }
+      }
+
+      var response = new Object();
+      response['total'] = carList[i].models.length;
+      response['marca'] = carList[i].brand;
+    };
+
+    moreModels.push(response);
+    res.json(moreModels);
+  } catch (error) {
+    res.json({message: error})
+    next();
+  }
+});
+
+route.get("listaMaisModelos/:qtd", (req, res, next) => {
+
 });
 
 export default route;
